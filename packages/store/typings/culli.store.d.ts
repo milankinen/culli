16b060597c92f85421addf5c759e5a3c18ef48f2
Matti Lankinen <m.lankinen@iki.fi>
2016-10-25 20:47:38 +0300

    fix(store): Fix typings

--- packages/store/typings/culli.store.d.ts
+++ packages/store/typings/culli.store.d.ts
@@ -1,49 +1,50 @@
 export declare type Observable<S> = any
 
-export declare type Action<A, S> = {
-    apply: (state:S) => S
-    value: A
-}
+export default function <S>(initialState:S):(actions:StoreSink<S>) => StoreSource<S>
 
-export declare type Selector = string | number
+export declare type StoreSource<S> = ItemSource<S> | ArraySource<S>
 
-export declare type Dispatcher<A, S> = (...actions:Array<Observable<A>>) => Observable<Action<A, S>>
+export declare type StoreSink<S> = Observable<Action<any>>
 
-export declare type ChildFn<S> = (item:ItemStore<S>, key?:string) => {[name: string]: Observable<any>}
 
-export interface ItemStoreValue<S> extends Observable<S> {
-    select:<SC> (selector:Selector) => Store<SC>
-}
-
-export interface ArrayStoreValue<S> extends ItemStoreValue<Array<S>> {
-    mapChildren: (childFn:ChildFn,
-                  eventSinks?:Array<string> = ["Store"],
-                  valueSinks?:Array<string> = ["DOM"]) => {[name: string]: Observable<any>}
-
-    mapChildrenById: (keyFn:(item:S) => string, childFn:ChildFn,
-                      eventSinks?:Array<string> = ["Store"],
-                      valueSinks?:Array<string> = ["DOM"]) => {[name: string]: Observable<any>}
-}
-
-export interface StoreActions<S> {
-    reduce:<A> (reducer:(state:S, action:A) => S) => Dispatcher<A, S>
-}
-
-export declare type ItemStore<S> = {
+export declare type ItemSource<S> = {
     value: ItemStoreValue<S>
     actions: StoreActions<S>
 }
 
-export declare type ArrayStore<S> = {
+export declare type ArraySource<S> = {
     value: ArrayStoreValue<S>
-    actions: StoreActions<S>
+    actions: StoreActions<Array<S>>
 }
 
-export declare type Store<S> = ItemStore<S> | ArrayStore<S>
+export interface ItemStoreValue<S> {
+    select:<SC> (selector:Selector) => StoreSource<SC>
+}
 
-export declare type StoreSource<S> = Store<S>
+export interface ArrayStoreValue<S> extends ItemStoreValue<Array<S>> {
+    mapChildren: (childFn:ChildFn<S>,
+                  eventSinks?:Array<string>,
+                  valueSinks?:Array<string>)
+        => {[name: string]: Observable<any>}
 
-export declare type StoreSink<A, S> = Observable<Action<A, S>>
+    mapChildrenBy: (keyFn:KeyFn<S>, childFn:ChildFn<S>, eventSinks?:Array<string>, valueSinks?:Array<string>)
+        => {[name: string]: Observable<any>}
+}
 
-export default function StoreDriver<A, S>(initialState:S):(actions:StoreSink<A, S>) => StoreSource<S>
+export interface StoreActions<S> {
+    reduce: (reducer:(state:S, action:any) => S) => DispatchFn<S>
+}
+
+export declare type Selector = string | number
+
+export declare type Action<S> = {
+    apply: (state:S) => S
+    value: any
+}
+
+export declare type DispatchFn<S> = (...actions:Array<Observable<any>>) => Observable<Action<S>>
+
+export declare type ChildFn<S> = (item:ItemSource<S>, key?:string) => {[name: string]: Observable<any>}
+
+export declare type KeyFn<S> = (item:S) => string
 
